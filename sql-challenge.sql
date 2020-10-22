0c700387c1698ee7bfb68235af2275c098eb6d35
Arvind Bhamidipati <arvindbhamidipati@users.noreply.github.com>
2020-10-21 17:13:37 -0700

    Answers 1 & 2

--- sql-challenge.sql
+++ sql-challenge.sql
@@ -53,3 +53,13 @@ REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_titles_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
+
+-- (1) List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees JOIN salaries
+ON employees.emp_no = salaries.emp_no;
+
+-- (2) List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employees.first_name, employees.last_name, employees.hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
