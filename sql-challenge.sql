338aed4f0251fd3a7e9ca01e3cd116561b925dbf
Arvind Bhamidipati <arvindbhamidipati@users.noreply.github.com>
2020-10-21 17:19:57 -0700

    Answers 3,4,5

--- sql-challenge.sql
+++ sql-challenge.sql
@@ -63,3 +63,23 @@ ON employees.emp_no = salaries.emp_no;
 SELECT employees.first_name, employees.last_name, employees.hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
+
+-- (3) List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
+
+-- (4) List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no;
+
+-- (5) List all employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
